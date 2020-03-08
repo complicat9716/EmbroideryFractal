abb740087bb5de797b1877048bead8843b248703
Complicated <cl3896@columbia.edu>
2020-03-07 21:10:47 -0500

    Circle

--- square5.py
+++ square5.py
@@ -1,3 +1,5 @@
+from math import *
+
 # Going through all four sides of the square
 # Clockwise, starting at the bottom left
 # Sides are 1 cm long, in ten stiches 1mm each
@@ -7,14 +9,67 @@ stitches = [128, 2,
 	    0, 0,
             206, 206,]
 
-for i in range(0, 10):
-    stitches += [10, 0,]
-for i in range(0, 10):
-    stitches += [0, 10,]
-for i in range(0, 10):
-    stitches += [246, 0,]
-for i in range(0, 10):
-    stitches += [0, 246,]
+# for i in range(0, 10):
+#     stitches += [10, 0,]
+# for i in range(0, 10):
+#     stitches += [0, 10,]
+# for i in range(0, 10):
+#     stitches += [246, 0,]
+# for i in range(0, 10):
+#     stitches += [0, 246,]
+
+##################################################################################################
+
+# control the step size
+stepsize = 0.1
+
+# starting point
+t = 0
+
+# end point
+end = 2*pi
+
+# scale factor
+scale = 10
+
+while t < end:
+
+    ################################
+    # x_axis
+    x_axis = int(scale*cos(t))
+
+    # Positive limit
+    if x_axis > 127:
+        print("X positive limit exceed!!")
+
+    if x_axis < 0:
+        x_axis = 256 + x_axis
+
+        # negative limit
+        if x_axis < 129:
+            print("X negative limit exceed!!")
+
+    ################################
+    # y_axis
+    y_axis = int(scale*sin(t))
+
+    # Positive limit
+    if y_axis > 127:
+        print("Y positive limit exceed!!")
+
+    if y_axis < 0:
+        y_axis = 256 + y_axis
+
+        # negative limit
+        if y_axis < 129:
+            print("Y negative limit exceed!!")
+
+    ################################
+    # stitch
+    stitches += [x_axis, y_axis,]
+    t = t + stepsize
+
+##################################################################################################
 
 stitches +=  [128, 16]         # "Last stitch" command code
 
@@ -53,5 +108,5 @@ jefBytes = [124, 0, 0, 0,   # The byte offset of the first stitch
  
 
 jefBytes = bytes(jefBytes)
-with open("square5_test.jef", "wb") as f:
+with open("circle1.jef", "wb") as f:
     f.write(jefBytes)
